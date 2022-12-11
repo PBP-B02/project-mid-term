86b9eda46d309570c673254f5e62264bf99b7fe2
bayurisma29 <maulana.bayu@ui.ac.id>
2022-12-11 19:26:35 +0700

    reverting to previous commit

--- project_django/settings.py
+++ project_django/settings.py
@@ -22,10 +22,6 @@ CORS_ALLOW_CREDENTIALS = True
 
 CORS_ALLOW_ALL_ORIGINS = True
 
-CORS_ALLOW_CREDENTIALS = True
-
-CORS_ALLOW_CREDENTIALS = True
-
 CSRF_COOKIE_SECURE = True
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SAMESITE = 'None'
@@ -48,11 +44,9 @@ INSTALLED_APPS = [
     'history',
     'article',
     'homepage',
-    'corsheaders',
 ]
 
 MIDDLEWARE = [
-    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
