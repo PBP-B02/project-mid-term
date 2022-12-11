ec224c4d0884cdca912afe5d2b871da95925767c
prometheuss420 <112608897+prometheuss420@users.noreply.github.com>
2022-12-12 01:12:12 +0700

    Update models.py

--- cashflow/models.py
+++ cashflow/models.py
@@ -2,14 +2,12 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Income(models.Model):
-    user = models.ForeignKey(User,on_delete=models.CASCADE)
     date = models.DateField()
     name = models.TextField()
     amount = models.IntegerField()
     income_type = models.TextField()
 
 class Spending(models.Model):
-    user = models.ForeignKey(User,on_delete=models.CASCADE)
     date = models.DateField()
     name = models.TextField()
     amount = models.IntegerField()
